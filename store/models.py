7a877d8aebab307df9e73a9bb3d911db64f09d92
naimulnobel <nobelnaimulislam@gmail.com>
2022-04-18 15:39:25 +0600

    model update

--- store/models.py
+++ store/models.py
@@ -3,12 +3,9 @@ from django.db import models
 # Create your models here.
 
 
-class Product(models.Model):
-    title = models.CharField(max_length=15)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventory = models.IntegerField()
-    last_updated = models.DateTimeField(auto_now=True)
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
 
 
 class Customer(models.Model):
